daac1ffbdea3bf257e77376836f4b7b673c6612e
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-21 09:48:20 +0530

    Update ConfFile_cfg.py

--- python/ConfFile_cfg.py
+++ python/ConfFile_cfg.py
@@ -24,8 +24,8 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 #process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi");
 #process.load("Geometry.CaloEventSetup.CaloGeometry_cfi");
 #process.load("Geometry.CaloEventSetup.CaloTopology_cfi");
-process.GlobalTag.globaltag = cms.string('80X_dataRun2_HLT_v12')
-process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
+#process.GlobalTag.globaltag = cms.string('80X_dataRun2_HLT_v12')
+#process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
 process.maxEvents = cms.untracked.PSet( 
     input = cms.untracked.int32(options.maxEvents) 
